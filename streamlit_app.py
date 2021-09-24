c7350f064ba9a7db3d175d69b1dc02ae88133467
Maarten Van den Bulcke <maarten.vdbul@gmail.com>
2021-09-24 10:49:46 +0200

    pimp the web app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,55 +1,104 @@
 import numpy as np
+import pandas as pd
 import streamlit as st
 from tensorflow.keras.models import load_model
 from PIL import Image
 import matplotlib.pyplot as plt
 
-
 from utils import data_preprocessing as dp
-
 
 if __name__ == '__main__':
 
-    model = load_model('model/v2_jacques.h5')
+    model = load_model('model/v3_mobilenetv2.h5')
 
-    explanation_expander = st.beta_expander('Some explanation on lesions')
+    st.set_page_config(page_title="skin cancer predictor", layout="wide")
+    hide_st_style = """ <style> footer {visibility: hidden;} </style> """
+
+    explanation_expander = st.beta_expander('Lesion legend')
     with explanation_expander:
-        st.write('try this')
+        st.markdown("<h3 style='text-align: left; color: #deb887; font-size:15px;'>* akiec: Actinic keratoses and intraepithelial carcinoma "
+                    "(Bowen\'s disease)</h3>",
+                    unsafe_allow_html=True)
+        st.markdown(
+            "<h3 style='text-align: left; color:grey; font-size:13px; margin-left:30px'>Variants of squamous cell"
+            " carcinoma that can be treated locally without surgery,"
+            "and commonly non-invasive", unsafe_allow_html=True)
+
+        st.markdown(
+            "<h3 style='text-align: left; color: #deb887; font-size:15px;'>* bcc: basal cell carcinoma",
+            unsafe_allow_html=True)
+        st.markdown("<h3 style='text-align: left;color:grey; font-size:13px; margin-left:30px'>A common variant of epithelial skin"
+                    " cancer that rarely metastasizes but grows destructively if untreated)", unsafe_allow_html=True)
+
+        st.markdown(
+            "<h3 style='text-align: left; color: #deb887; font-size:15px;'>* bkl: benign keratosis-like lesions",
+            unsafe_allow_html=True)
+        st.markdown("<h3 style='text-align: left;color:grey; font-size:13px; margin-left:30px'"
+                    ">They are similar biologically and often reported under the same generic term histopathologically",
+                    unsafe_allow_html=True)
+
+        st.markdown(
+            "<h3 style='text-align: left; color: #deb887; font-size:15px;'>* df: dermatofibroma ",
+            unsafe_allow_html=True)
+        st.markdown("<h3 style='text-align: left;color:grey; font-size:13px; margin-left:30px'"
+                    ">Benign skin lesion regarded as either a benign proliferation or an inflammatory reaction"
+                    " to minimal trauma",
+                    unsafe_allow_html=True)
+
+        st.markdown(
+            "<h3 style='text-align: left; color:grey; color: #deb887; font-size:15px;'>* nv: melanocytic nevi  ",
+            unsafe_allow_html=True)
+        st.markdown("<h3 style='text-align: left; color: grey; font-size:13px; margin-left:30px'"
+                    ">Benign neoplasms of melanocytes, which appear in many variants",
+                    unsafe_allow_html=True)
+
+        st.markdown(
+            "<h3 style='text-align: left; color:grey; color: #deb887; font-size:15px;'>* vasc: vascular lesions  ",
+            unsafe_allow_html=True)
+        st.markdown("<h3 style='text-align: left; color: grey; font-size:13px; margin-left:30px'"
+                    ">Vascular lesions, which are pigmented by hemoglobin and not by melanin",
+                    unsafe_allow_html=True)
+
+
+        st.markdown(
+            "<h3 style='text-align: left; color: #deb887; font-size:15px;'>* mel: melanoma  ",
+            unsafe_allow_html=True)
+        st.markdown("<h3 style='text-align: left;color:grey; color: grey; font-size:13px; margin-left:30px'"
+                    ">The deadliest type of skin cancer. It  is a malignant neoplasm derived"
+                    " from melanocytes that may appear in different variants",
+                    unsafe_allow_html=True)
+
 
     st.title('Skin cancer detector')
 
     uploaded_file = st.file_uploader('Upload an lesion image')
     if uploaded_file is not None:
-        valid_file = False
+        # valid_file = False
         try:
             img = Image.open(uploaded_file)
-            st.write(type(img))
-            valid_file = True
-
-            # plot
-            # fig, ax = plt.subplots()
-            # plt.imshow(img)
-            # st.pyplot(fig)
-
-            prepared_image = dp.prepare_one_image_no_tl(img)
-
-            pred_array = model.predict(prepared_image)
-            st.write(pred_array)
-            pred_index = np.argmax(pred_array)
+            # valid_file = True
 
             classes = ['akiec', 'bcc', 'bkl', 'df', 'nv', 'vasc', 'mel']
             cancerous_classes = ['mel', 'bkl', 'bcc']  # bkl = warning
 
-            predicted_class = classes[pred_index]
+            prepared_image = dp.prepare_one_image_no_tl(img)
+            pred_array = model.predict(prepared_image)
 
-            st.write('This is the predicted class:', predicted_class)
+            pred_array_df = pd.DataFrame(pred_array, columns=classes)
+            st.write(pred_array_df)
+
+            pred_index = np.argmax(pred_array)
+            predicted_class = classes[pred_index]
+            st.write('The predicted lesion type:', predicted_class)
             if predicted_class in cancerous_classes:
-                st.write('Bad news')
+                st.write('Diagnosis: cancerous')
             else:
-                st.write('Good news')
+                st.write('Diagnosis: not cancerous')
+
+
         except:
             st.write('Sorry but we do not understand the uploaded file.'
-                     'Please make sure to upload an image file.')
+                     ' Please make sure to upload an image file.')
 
     hide_st_style = """ <style> footer {visibility: hidden;} </style> """
     st.markdown(hide_st_style, unsafe_allow_html=True)
--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -192,3 +192,16 @@ def remove_hair(img):
     dst = dst[:, :, ::-1]
     # cv2.imshow('no hair image?', dst)
     return dst
+
+
+def prepare_one_image_no_tl(img):
+    tf_image = np.array(img)
+    img_resized = np.resize(tf_image, (224, 224, 3))
+    # st.write(img_resized.shape)
+    img_resized = img_resized[:, :, ::-1]  # RGB to BGR
+    img_reshaped = img_resized.reshape(
+        (1, img_resized.shape[0], img_resized.shape[1], img_resized.shape[2]))
+    # st.write(img_reshaped.shape)
+    img_scaled = img_reshaped / 255
+    # st.write(img_scaled)
+    return img_scaled
